09f531ed3a74c8011c525af794fd910d04babc3e
Anushka-Bhati7 <anushkakanwarbhati7@gmail.com>
2026-04-01 16:41:53 +0530

    Modify app.run to use dynamic port from environment
    Update Flask app to use environment variable for port.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+import os
 from ml_model import predict_ml
 from recommender import refine_with_rules
 from optimizer import optimize_budget
@@ -70,5 +71,6 @@ def chat():
         "insights": insights
     })
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
